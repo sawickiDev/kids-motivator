cb47dbd06b18a8ac67890ebcf23fab7091ba28cd
sawickiDev <sawicki.dev@gmail.com>
2018-05-01 22:31:17 +0200

    persistance init

---
+++ src/main/java/com/steveq/kidsmotivator/login/LoginController.java
@@ -0,0 +1,12 @@
+package com.steveq.kidsmotivator.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String
+
+}
