c78c292e45ed7bc616302e10cf4b9322c41c5558
sawickiDev <sawicki.dev@gmail.com>
2018-05-01 19:59:26 +0200

    Initial commit

---
+++ src/main/java/com/steveq/kidsmotivator/KidsMotivatorApplication.java
@@ -0,0 +1,12 @@
+package com.steveq.kidsmotivator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsMotivatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidsMotivatorApplication.class, args);
+    }
+}
